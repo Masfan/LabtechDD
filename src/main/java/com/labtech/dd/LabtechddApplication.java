46b2629241e4d9af04ce0beea11424c096bb9bad
Masfan <Masfan@bishop.zj.github.com>
2020-07-28 11:02:58 +0800

    1

---
+++ src/main/java/com/labtech/dd/LabtechddApplication.java
@@ -0,0 +1,13 @@
+package com.labtech.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabtechddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabtechddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labtech/dd/LabtechddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labtech.dd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabtechddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
